1e97a5fef0470bdecd60c1bb0d65db2b24d84271
Harish <harishkandukuri23@gmail.com>
2026-07-16 18:47:09 +0530

    Time: 51 ms (15.67%), Space: 19.2 MB (68.92%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -10,7 +10,7 @@ class Solution:
         low=1
         high=n
         while(low<=high):
-            mid=low+(high-low)//2
+            mid=(low+high)//2
             res=guess(mid)
             if res==0:
                 return mid
